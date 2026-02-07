10b35efb25664649205f421d056ed14604d16192
Pawlo83 <94640915+Pawlo83@users.noreply.github.com>
2026-02-07 23:58:26 +0100

    Update copyright year in config.h
    Update copyright year from 2022 to 2026.

--- keyboards/handwired/pscottofly/keymaps/default/config.h
+++ keyboards/handwired/pscottofly/keymaps/default/config.h
@@ -1,5 +1,6 @@
 /*
 Copyright 2022 Joe Scotto
+Copyright 2026 Pawlo83
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
